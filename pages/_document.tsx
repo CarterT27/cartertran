14cafab9ca9db3630ab17187aacbb0592df56bb0
CarterT27 <carter.tran@gmail.com>
2022-01-21 21:11:35 -0700

    feat: :sparkles: Favicon, minimal home page
    Added favicons for different browsers and a changed the home page from the basic template

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
     return { ...initialProps }
   }
@@ -9,7 +9,12 @@ class MyDocument extends Document {
   render() {
     return (
       <Html data-theme="dracula">
-        <Head />
+        <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+        </Head>
         <body>
           <Main />
           <NextScript />
